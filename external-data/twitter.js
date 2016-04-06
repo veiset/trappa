00a2f773984fb26c7dadf0ee76eedd0443dacfab
larseliassen <lars.eliassen@bekk.no>
2016-04-06 13:20:02 +0200

    Update twitter.js

--- external-data/twitter.js
+++ external-data/twitter.js
@@ -9,7 +9,7 @@ var twitter = new Twitter({
 var Firebase = require('firebase');
 var firebase = new Firebase("https://bekk-trappa.firebaseio.com/");
 
-var stream = twitter.stream('statuses/filter', { track: 'fuck'});
+var stream = twitter.stream('statuses/filter', { track: 'hashtag'});
 
 stream.on('tweet', function(tweet) {
     console.log(tweet);
